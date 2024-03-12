01c278bd02e617d44ad455052eaa733da71b853a
Martin-Molinero <martin.molinero1@gmail.com>
2024-03-12 19:51:32 -0300

    Update rawsync.sh

--- DataProcessing/rawsync.sh
+++ DataProcessing/rawsync.sh
@@ -3,7 +3,7 @@
 current_date=$(date +%Y%m%d)
 mkdir -p /raw/equity/usa/shortable/interactivebrokers/$current_date
 
-lftp -u shortstock,anonymous ftp://ftp3.interactivebrokers.com <<EOF
+lftp -u shortstock,anonymous ftp://ftp2.interactivebrokers.com <<EOF
 mirror --target-directory /raw/equity/usa/shortable/interactivebrokers/$current_date -v
 exit
 EOF
